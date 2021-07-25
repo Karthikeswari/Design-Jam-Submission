92848b396e4faff0baa16c0499191c5d940523b1
Karthikeswari Padmanaban <81372980+Karthikeswari@users.noreply.github.com>
2021-07-25 11:09:29 +0530

    Updated

--- guess.py
+++ guess.py
@@ -2,19 +2,31 @@ from random import randint
 
 start = 1
 end = 100
+guess = None
+count = 1
+points = 100
 value = randint(start, end)
-print("clue: ",bin(value))
 print("")
 print("I'm thinking of a number between", start, "and", end)
 print("")
-
-guess = None
-count = 1
+print("if you need clue,Press 1")
+clue = int(input())
+if clue == 1:
+    print("To Continue with clue by giving 10 of your points press 1 again else print 0")
+    y = int(input())
+    if y == 1:
+        print("clue: ",bin(value))
+        points = points - 10
+    else:
+        print("Continue guessing without clue")
+else:
+    print("Continue guessing without clue")
 while guess != value:
     print("You are Trying for the %d time" % count)
     print("")
 
     count = count + 1
+    points = points - 1
     text = int(input("Guess the number: "))
     print("")
 
@@ -27,8 +39,10 @@ while guess != value:
     elif guess > value:
         print("Try Lower Number.")
         print("")
-
-    
 print("Congratulations! You guessed the right answer %d in %d trys" % (value,count - 1))
-print("")
+print("Your points",points)
+if points < 93:
+    print("!!!could be better!!! - Thank you")
+elif points >= 93:
+    print("Good guess- Thank you")
 
